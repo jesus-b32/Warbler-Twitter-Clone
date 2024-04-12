080539211f870f7ea06b6c027aefaf200bef7057
jesus <149453045+jesus-b32@users.noreply.github.com>
2024-04-11 19:28:41 -0700

    created database; had to change seed.py file to add data to database without error

--- seed.py
+++ seed.py
@@ -1,20 +1,27 @@
 """Seed database with sample data from CSV Files."""
 
 from csv import DictReader
-from app import db
+from app import db, app
 from models import User, Message, Follows
 
-
-db.drop_all()
-db.create_all()
+with app.app_context():
+    db.drop_all()
+    db.create_all()
 
 with open('generator/users.csv') as users:
-    db.session.bulk_insert_mappings(User, DictReader(users))
+    with app.app_context():
+        db.session.bulk_insert_mappings(User, DictReader(users))
+        db.session.commit()
 
 with open('generator/messages.csv') as messages:
-    db.session.bulk_insert_mappings(Message, DictReader(messages))
+    with app.app_context():
+        db.session.bulk_insert_mappings(Message, DictReader(messages))
+        db.session.commit()
 
 with open('generator/follows.csv') as follows:
-    db.session.bulk_insert_mappings(Follows, DictReader(follows))
+    with app.app_context():
+        db.session.bulk_insert_mappings(Follows, DictReader(follows))
+        db.session.commit()
 
-db.session.commit()
+# with app.app_context():
+#     db.session.commit()
